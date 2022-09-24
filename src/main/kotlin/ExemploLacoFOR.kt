5ec33816c8769a9d0eae45d967e5bc19fc413b00
dfmoura <diogomou@gmail.com>
2022-09-24 16:05:24 -0300

    Initial commit

---
+++ src/main/kotlin/ExemploLacoFOR.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    for (i in 1..10 ){
+
+        println("Valor do i no laço vale: $i")
+    }
+
+    for (i in 1..10 step 2){
+
+        println("Valor do i no laço vale: $i")
+    }
+
+    for (i in 10 downTo 0 step 2){
+
+        println("Valor do i no laço vale: $i")
+    }
+
+    for (i in 10 downTo 0){
+
+        println("Valor do i no laço vale: $i")
+    }
+}
---
+++ src/main/kotlin/ExemploLacoWHILE.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+    var i = 0
+    var a = 0
+    var b = 10
+
+    while (i<=10) {
+
+        println("Valor do i no laço vale: $i")
+
+        i++ // i = i + 1
+    }
+
+    while (a<=10) {
+
+        println("Valor do a no laço vale: $a")
+
+        a = a + 2
+    }
+
+
+    while (b>=1) {
+
+        println("Valor do b no laço vale: $b")
+
+        b-- // b = b - 1
+
+    }
+
+
+
+}
